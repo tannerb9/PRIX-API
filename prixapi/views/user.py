1d4d855898ca9f4cfd3296d3a4a82d4731584525
tannerb9 <tanner-brainard@hotmail.com>
2020-09-11 21:50:29 -0500

    feat: created UserSerializer and UserView

---
+++ prixapi/views/user.py
@@ -0,0 +1,19 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = User
+        url = serializers.HyperlinkedIdentityField(
+            view_name='user',
+            lookup_field='id'
+        )
+        fields = ('id', 'url', 'first_name', 'last_name', 'email')
+
+
+class UserView(ModelViewSet):
+
+    queryset = User.objects.all()
